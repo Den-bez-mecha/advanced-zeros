3ea5900b7b00fd239ef65dfa0347eb44c67d0581
Den Havanski <havanski.denis@gmail.com>
2018-03-14 00:21:02 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+function getQuotient(number, base, exp) {
+  var quotient = number / Math.pow((0.5 * base), exp);
+  return quotient;
+}
+
+function getInteger(quotient) {
+  var integer = Math.floor(quotient);
+  return integer;
+}
+
+function getZeros(zeroCount, integer) {
+  return zeroCount + integer;
+}
+
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var exp = 1;
+  var zerosCount = 0;
+
+   quot = getQuotient(number, base, exp);
+  if (quot < 1) {
+    return zerosCount;
+  } else {
+      integer = getInteger(quot);
+      while (integer >= parseInt((0.5 * base))) {
+        zerosCount = getZeros(zerosCount, integer);
+        exp++;
+        quot = getQuotient(number, base, exp);
+        integer = getInteger(quot);
+      }
+    zerosCount = getZeros(zerosCount, integer);
+  }
+  return zerosCount;
 }
